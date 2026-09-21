05db12a76c955b312460a176c7113c4bd1d24070
Barsha Bhakat <barshabhakat27@gmail.com>
2026-09-21 13:03:08 +0530

    Add files via upload

---
+++ Barsha BCA (26).cpp
@@ -0,0 +1,11 @@
+//w.c.p to print 1 to 5 using while loop
+#include<stdio.h>
+int main() {
+   int n=1;
+   while(n<=5){
+   	printf("%d\n", n);
+   	n++;
+   }
+   return 0;	
+}
+
---
+++ barsha.c
@@ -0,0 +1,11 @@
+//w.c.p to print odd numbes upto 10
+#include<stdio.h>
+int main() {
+   int n=1;
+   while(n<=20){
+   	printf("%d\n", n);
+   	n+=2;
+   }
+   return 0;	
+}
+
